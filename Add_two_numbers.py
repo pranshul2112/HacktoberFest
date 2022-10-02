b3ad817a99fcbf1ca930767f3c691e9a72b81659
Evan Knapke <evanknapke@gmail.com>
2022-10-02 12:30:34 -0400

    Refactor and improve adding numbers program

--- Add_two_numbers.py
+++ Add_two_numbers.py
@@ -1,5 +1,10 @@
-a= int(input())
-b= int(input())
-summm=a+b
-print(summm)
+def main():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    answer = summation(a, b)
+    print("\nThe summation of", a, "and", b, "is", answer)
 
+def summation(num1, num2):
+    return num1 + num2
+
+main()
